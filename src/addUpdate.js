e3d6e7b45298bb6eb6eeac732f5cec611084712a
Matt Anderson <mpanderson06@fastmail.com>
2023-03-28 21:10:25 -0500

    Can now add to inventory

--- src/addUpdate.js
+++ src/addUpdate.js
@@ -1,3 +1,4 @@
+const g = require('./global')
 const express = require('express')
 const router = express.Router()
 
@@ -6,6 +7,29 @@ router.use((req, res, next) => {
 });
 
 router.get('/', (req, res) => {
+	res.render('addUpdate')
+});
+
+router.post('/', (req, res) => {
+	const opt = { qos: 2 };
+	const obj = {
+		ItemNumber: Number(req.body.itemNumber),
+		CurrentQty: req.body.currentQty,
+		Description: req.body.description,
+		SourceUrl: req.body.sourceUrl,
+		InventoryType: Number(req.body.inventoryType),
+		Manufacturer: req.body.manufacturer,
+		PartNumber: req.body.partNumber,
+		MinQty: Number(req.body.minQty),
+		Location: req.body.location
+	}
+	g.Globals.mqttClient.publish(g.Globals.addUpdateItemTopic, JSON.stringify(obj), opt, function (err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+
+	// Go back to the main addUpdate page
 	res.render('addUpdate')
 });
 
