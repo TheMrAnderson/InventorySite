04d38de4f8ca475ad4325135ccfe4ce5dc7bedd6
Matt Anderson <mpanderson06@fastmail.com>
2024-04-16 15:45:52 -0500

    Removed the nav menu and simplified navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,32 +7,19 @@ export default function Nav({ title }) {
       <nav className="navbar">
         <h2>{title}</h2>
         <title>React Inventory | {title}</title>
-        {/* <!-- NAVIGATION MENU --> */}
         <ul className="nav-links">
-          {/* <!-- USING CHECKBOX HACK --> */}
-          <input type="checkbox" id="checkbox_toggle" />
-          <label htmlFor="checkbox_toggle" className="hamburger">
-            &#9776;
-          </label>
-          {/* <!-- NAVIGATION MENUS --> */}
-          <div className="menu">
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <li><Link to="/shoppingList">Shopping List</Link></li>
-            </li>
-            <li className="services">
-              <a href="/">Actions</a>
-              {/* <!-- DROPDOWN MENU --> */}
-              <ul className="dropdown">
-                <li>
-                  <Link to="/addUpdate">Add</Link>
-                </li>
-                <li><Link to="/consume">Consume</Link></li>
-              </ul>
-            </li>
-          </div>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <li><Link to="/shoppingList">Shopping List</Link></li>
+          </li>
+          <li>
+            <Link to="/addUpdate">Add</Link>
+          </li>
+          <li>
+            <Link to="/consume">Consume</Link>
+          </li>
         </ul>
       </nav>
     </>
